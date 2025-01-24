2e5406dea0d71c1716c69977ac3eee8681a6f8d6
ᴩʜᴏɴᴇᴅʀᴏɪᴅ <73050054+PhoneDroid@users.noreply.github.com>
2025-01-24 07:18:14 -0500

    Quartz setup

---
+++ quartz/i18n/index.ts
@@ -0,0 +1,14 @@
+import { Translation, CalloutTranslation } from "./locales/definition"
+import en from "./locales/en-US"
+import ja from "./locales/ja-JP"
+
+
+export const TRANSLATIONS = {
+  "en-US": en,
+  "ja-JP": ja
+} as const
+
+export const defaultTranslation = "en-US"
+export const i18n = (locale: ValidLocale): Translation => TRANSLATIONS[locale ?? defaultTranslation]
+export type ValidLocale = keyof typeof TRANSLATIONS
+export type ValidCallout = keyof CalloutTranslation
